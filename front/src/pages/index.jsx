ed55ac5a547668607b6824bffe45ac9b229d6b87
Maiterney <43651018+Maiterney@users.noreply.github.com>
2021-09-21 21:04:47 -0300

    create front end
    create front end

---
+++ front/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main className="main">
+        <Header />
+      </main>
+    </div>
+  )
+}
